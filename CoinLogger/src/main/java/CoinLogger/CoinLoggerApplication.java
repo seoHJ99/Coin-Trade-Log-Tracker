cac60766d96631bc0eb44ede5c89c1ff28a07820
MakingNameIsAnnoying <9669579@naver.com>
2023-06-08 22:17:02 +0900

    스프링 파일 생성
    내부에 업비트 조회 기능 추가

---
+++ CoinLogger/src/main/java/CoinLogger/CoinLoggerApplication.java
@@ -0,0 +1,12 @@
+package CoinLogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinLoggerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CoinLoggerApplication.class, args);
+	}
+
+}
---
+++ CoinLogger/src/test/java/CoinLogger/CoinLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package CoinLogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

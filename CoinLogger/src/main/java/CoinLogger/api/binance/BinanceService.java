d647cd00185a5377201657525fb28a15a6dc80ee
MakingNameIsAnnoying <121778872+MakingNameIsAnnoying@users.noreply.github.com>
2023-06-25 23:50:39 +0900

    내 코인 현재 가격 조회 방법 변경 결정
    아직 미구현.
    코인 이름으로 차트 조회 돌아야 할듯.
    계좌 정보 가져오기는 실시간 변동이 안됨

--- CoinLogger/src/main/java/CoinLogger/api/binance/BinanceService.java
+++ CoinLogger/src/main/java/CoinLogger/api/binance/BinanceService.java
@@ -56,17 +56,20 @@ public class BinanceService {
         return result;
     }
 
-    public double getBTCValue() throws IOException, ParseException {
-        HttpGet request = new HttpGet(serverUrl + "/api/v3/avgPrice?symbol=BTCUSDT");
-        request.addHeader("Content-Type", "application/json");
-        request.addHeader("X-MBX-APIKEY", accessKey);
-        HttpResponse response = httpClient.execute(request);
-        HttpEntity entity = response.getEntity();
-        String result = EntityUtils.toString(entity, "UTF-8");
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
-        jsonObject = new JSONObject(jsonObject);
-        return Double.valueOf((String) jsonObject.get("price"));
-    }
+//
+//    public double getBTCValue() throws IOException, ParseException {
+//        HttpGet request = new HttpGet(serverUrl + "/api/v3/avgPrice?symbol=BTCUSDT");
+//        request.addHeader("Content-Type", "application/json");
+//        request.addHeader("X-MBX-APIKEY", accessKey);
+//        HttpResponse response = httpClient.execute(request);
+//        HttpEntity entity = response.getEntity();
+//        String result = EntityUtils.toString(entity, "UTF-8");
+//        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+//        jsonObject = new JSONObject(jsonObject);
+//        return Double.valueOf((String) jsonObject.get("price"));
+//    }
+    // 코인 이름으로 차트 조회 돌아야 할듯.
+    // 계좌 정보 가져오기는 실시간 변동이 안됨
 
    public void coinBTCValueToWon(List<List<String>> myCoin) throws IOException, ParseException {
         if(oneDollarWon == 0){
@@ -80,13 +83,13 @@ public class BinanceService {
             JSONObject a = (JSONObject)jsonObject.get(0);
             oneDollarWon = (double) a.get("basePrice");
         }
-        double btc = getBTCValue();
+//        double btc = getBTCValue();
         for(int i =0; i<myCoin.size(); i++){
             double coinBtcValue = Double.valueOf( myCoin.get(i).get(6));
             System.out.println(myCoin.get(i).get(0));
             System.out.println("coinBtcValue = " + coinBtcValue);
-            double price = btc  * coinBtcValue;
-            System.out.println("price = " + price);
+//            double price = btc  * coinBtcValue;
+//            System.out.println("price = " + price);
 
         }
         return ;

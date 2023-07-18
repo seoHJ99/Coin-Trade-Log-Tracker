e1969539c3fa9654cedb3c98b0818da7ed71ef70
MakingNameIsAnnoying <9669579@naver.com>
2023-07-18 14:55:51 +0900

    Update HashingTest.java

--- CoinLogger/src/test/java/CoinLogger/api/HashingTest.java
+++ CoinLogger/src/test/java/CoinLogger/api/HashingTest.java
@@ -7,11 +7,4 @@ import javax.crypto.Mac;
 import static org.junit.Assert.*;
 
 public class HashingTest {
-    public void testEmptyInputs() throws Exception {
-        String knownOutput = "8cbf764cbe2e4623d99a41354adfd390";
-        Mac mac = Mac.getInstance("HmacMD5");
-        mac.init(MD5_KEY);
-        assertEquals(knownOutput, HashCode.fromBytes(mac.doFinal()).toString());
-        assertEquals(knownOutput, Hashing.hmacMd5(MD5_KEY).newHasher().hash().toString());
-    }
 }

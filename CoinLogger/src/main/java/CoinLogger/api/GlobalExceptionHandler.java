ba5415fee0f1a96701ec27e7aef9da84aecdea58
MakingNameIsAnnoying <9669579@naver.com>
2023-07-25 19:48:44 +0900

    Create GlobalExceptionHandler.java

---
+++ CoinLogger/src/main/java/CoinLogger/api/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package CoinLogger.api;
+
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ParseException.class)
+    public Object parseExceptionHandler(Exception e){
+        e.printStackTrace();
+        return ResponseEntity.ok("<script>alert('파싱 에러! 개발자에게 연락하세요!')</script>");
+    }
+
+    @ExceptionHandler(value = ClassCastException.class)
+    public Object classCastExceptionHandler(Exception e){
+        e.printStackTrace();
+        return ResponseEntity.ok("<script>alert('변환 에러!" +
+                "1. 업비트 api ip주소가 옳바른지 확인해주세요" +
+                "2. api key가 옳바른지 확인해주세요" +
+                "3. 해결법이 통하지 않으면 개발자에게 연락하세요!')</script>");
+    }
+}
